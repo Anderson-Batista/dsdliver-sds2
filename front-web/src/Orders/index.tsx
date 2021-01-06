85fabe965634bb6b0c896dd49195f72ce7ecc127
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-01-06 18:28:26 -0300

    Add orders cards/price

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,14 +1,25 @@
-import StepsHeader from './StepsHeader';
+import { useEffect, useState } from 'react';
 import './styles.css';
-
+import ProductsLists from './ProductsLists';
+import StepsHeader from './StepsHeader';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+       fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error)) 
+    }, []);
+
     return (
        <div className="orders-container">
            <StepsHeader />
+           <ProductsLists products={products} />
        </div>
-    )
-    
+    ) 
 }
 
 export default Orders;
